36f937cab06699eb7a75e22a140a384a61093834
pablogarciamiranda <uo245530@uniovi.es>
2017-03-06 17:39:03 +0100

    Tasks can be added

--- src/uo/sdi/presentation/BeanTask.java
+++ src/uo/sdi/presentation/BeanTask.java
@@ -1,19 +1,40 @@
 package uo.sdi.presentation;
 
 import java.io.Serializable;
+import java.util.Date;
+
+import javax.annotation.PostConstruct;
+import javax.enterprise.context.RequestScoped;
+import javax.faces.bean.ManagedBean;
+import javax.faces.context.FacesContext;
 
 import uo.sdi.business.Services;
 import uo.sdi.business.TaskService;
 import uo.sdi.business.exception.BusinessException;
 import uo.sdi.dto.Task;
+import uo.sdi.dto.User;
 
-public class BeanTask extends Task implements Serializable {
+@ManagedBean(name = "task")
+@RequestScoped
+public class BeanTask implements Serializable {
 
 	private static final long serialVersionUID = 1L;
+	private User user;
+	private Long id;
+	private String title, comments;
+	private Date created, planned, finished;
+	private Long categoryId, userId;
 
 	public BeanTask() {
 	}
 
+	@PostConstruct
+	public void init(){
+		user = (User) FacesContext.getCurrentInstance()
+				.getExternalContext().getSessionMap().get("LOGGEDIN_USER");
+		
+	}
+	
 	public void showTask(Task task) {
 		setId(task.getId());
 
@@ -23,8 +44,6 @@ public class BeanTask extends Task implements Serializable {
 		setCreated(task.getCreated());
 		setPlanned(task.getPlanned());
 		setFinished(task.getFinished());
-		
-		//User inyeccion de dependencia???
 		setUserId(task.getUserId());
 	}
 
@@ -34,7 +53,7 @@ public class BeanTask extends Task implements Serializable {
 		task.setTitle(getTitle());
 		
 		//Inyeccion de dependencia???
-		task.setUserId(getUserId());
+		task.setUserId(user.getId());
 
 		// Task is registered in db
 		TaskService taskService = Services.getTaskService();
@@ -82,6 +101,78 @@ public class BeanTask extends Task implements Serializable {
 			return null;
 		}
 		return "exito";	
+	}
+
+	public User getUser() {
+		return user;
+	}
+
+	public void setUser(User user) {
+		this.user = user;
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getTitle() {
+		return title;
+	}
+
+	public void setTitle(String title) {
+		this.title = title;
+	}
+
+	public String getComments() {
+		return comments;
+	}
+
+	public void setComments(String comments) {
+		this.comments = comments;
+	}
+
+	public Date getCreated() {
+		return created;
+	}
+
+	public void setCreated(Date created) {
+		this.created = created;
+	}
+
+	public Date getPlanned() {
+		return planned;
+	}
+
+	public void setPlanned(Date planned) {
+		this.planned = planned;
+	}
+
+	public Date getFinished() {
+		return finished;
+	}
+
+	public void setFinished(Date finished) {
+		this.finished = finished;
+	}
+
+	public Long getCategoryId() {
+		return categoryId;
+	}
+
+	public void setCategoryId(Long categoryId) {
+		this.categoryId = categoryId;
+	}
+
+	public Long getUserId() {
+		return userId;
+	}
+
+	public void setUserId(Long userId) {
+		this.userId = userId;
 	}
 
 
